663cbc1178407b45962f938b136ca7f57f8986cc
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-07-22 07:46:44 +0300

    sample_problem_1: add function declarations (HTDP wish list)

--- sample_problems/01_max_show_profit.rkt
+++ sample_problems/01_max_show_profit.rkt
@@ -9,3 +9,16 @@
 ; 
 ; fxdPerfCostAtt :: Float
 ; **Fixed Performance Cost Per Attendee** : additional expenses per attendee.
+
+; Natural Float Float Float -> Float
+; Given numAtt, tPrice, fxdPerfCost, fxdPerfCostAtt, calculates a profit value of one performance.
+(define calcPerfProfit (numAtt, tPrice, fxdPerfCost, fxdPerfCostAtt)
+; function body
+  )
+
+; Natural Float Float -> Float
+; Given numAtt, fxdPerfCost, fxdPerfCostAtt, calculates how much one performance costs to the owner.
+; This is a helper function.
+(define calcPerfExpenses (numAtt, fxdPerfCost, fxdPerfCostAtt)
+; function body
+  )
