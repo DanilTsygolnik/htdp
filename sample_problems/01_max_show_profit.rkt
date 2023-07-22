b16104b4244e924cf8f2a222fa76453f8bb0466d
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-07-22 08:34:37 +0300

    fix calcPerfProfit unit tests (to squash)

--- sample_problems/01_max_show_profit.rkt
+++ sample_problems/01_max_show_profit.rkt
@@ -14,7 +14,7 @@
 ; Natural Float Float Float -> Float
 ; Given numAtt, tPrice, fxdPerfCost, fxdPerfCostAtt, calculates a profit value of one performance.
 
-(check-expect (calcPerfProfit 0 50 20 1) 0)
+(check-expect (calcPerfProfit 0 50 20 1) -20)
 (check-expect (calcPerfProfit 1 50 20 1) (- (* 1 50) (calcPerfExpenses 1 20 1)))
 
 (define (calcPerfProfit numAtt tPrice fxdPerfCost fxdPerfCostAtt)
