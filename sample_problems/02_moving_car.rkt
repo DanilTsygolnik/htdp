324004ebe15cc416cf974213ac0af62a9ba817e0
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-07-22 21:20:00 +0300

    sample_problem_2pt1: add input data description (16m)

---
+++ sample_problems/02_moving_car.rkt
@@ -0,0 +1,10 @@
+; scHgtPx, scWthPx, carPosX, carImgHgtPx :: Number
+; **Scene Height, Scene Width** : scene image dimensions in pixels.
+; **Car Position X** : the car's position on the x-coordinate line ranges from 0 pixels to the width value of the scene.
+; **Car Image Height Pixels** : the image should be resized to this specified height in pixels.
+; 
+; carImgPath :: String
+; **Car Image Path** : filepath to image file.
+; 
+; sc, car, scWithCar :: Image
+; **Scene, Car Image, Scene With Car**
