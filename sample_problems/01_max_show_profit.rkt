acb1cabec21f1a7cf72372b2c2bc0687cffdf5b9
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-07-22 07:34:50 +0300

    sample_problem_1: add input data description (18m)

---
+++ sample_problems/01_max_show_profit.rkt
@@ -0,0 +1,11 @@
+; numAtt :: Natural
+; **Number of Attendees** : how many people come to the show.
+; 
+; tPrice :: Float
+; **Ticket Price**
+; 
+; fxdPerfCost :: Float
+; **Fixed Performance Expenses** : how much hosting one show costs to the owner.
+; 
+; fxdPerfCostAtt :: Float
+; **Fixed Performance Cost Per Attendee** : additional expenses per attendee.
